5b9db67201963286401b1e13d6d47114032f2ca9
ynsmrtpc <yunusemre_topcu@hotmail.com>
2023-06-08 23:16:26 +0300

    added ip address

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,26 @@
-import Image from 'next/image'
-// import { Inter } from 'next/font/google'
-//
-// const inter = Inter({ subsets: ['latin'] })
-import Head from 'next/head';
-import {useEffect} from "react";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
 
-export default function Home() {
-        return (
-        <>
-            <Head>
-                <title>Home - Avalon Admin Panel</title>
-            </Head>
-            sa
-        </>
-    )
-}
+export default  function Home() {
+    const [ipAddress, setIpAddress] = useState('');
+
+    useEffect(() => {
+        const fetchIpAddress = async () => {
+            try {
+                const response = await fetch('https://api.ipify.org?format=json');
+                const data = await response.json();
+                setIpAddress(data.ip);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchIpAddress();
+    }, []);
+
+    return (
+        <div>
+            <h1>Your IP Address: {ipAddress}</h1>
+        </div>
+    );
+};
+
