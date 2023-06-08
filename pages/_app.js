2d22f3aad6ae22f164372d69d25ae50214edfa0a
ynsmrtpc <yunusemre_topcu@hotmail.com>
2023-06-08 19:51:58 +0300

    creating sidebar layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css'
+import HomeLayout from '@/layouts/HomeLayout.js'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}) {
+    const getLayout = (Component) => {
+        return HomeLayout;
+    };
+    const Layout = getLayout(Component);
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    )
 }
