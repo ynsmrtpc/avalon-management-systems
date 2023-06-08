9c0e47dd3bafb3a8e683e4c8517b54d822fcf293
ynsmrtpc <yunusemre_topcu@hotmail.com>
2023-06-08 19:51:58 +0300

    creating sidebar layout

---
+++ layouts/HomeLayout.js
@@ -0,0 +1,137 @@
+import { Inter } from 'next/font/google'
+import Link from "next/link";
+import classNames from "classnames";
+import {useState} from "react";
+import Head from 'next/head';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function HomeLayout({ children }) {
+
+    const [profileToggle, setProfileToggle] = useState(false);
+    const [sidebarToggle, setSidebarToggle] = useState(false);
+
+    const profileHandle = () => {
+        setProfileToggle(!profileToggle);
+    }
+    const sidebarHandle = () => {
+        setSidebarToggle(!sidebarToggle);
+    }
+
+    return (
+        <>
+            <Head>
+                <meta name="description" content="Avalon Admin Panel, güçlü ve kullanıcı dostu bir yönetim panelidir. Projenizi kolayca yönetebilir, kullanıcıları yönetebilir, veri tabanınızı yönetebilir ve daha fazlasını yapabilirsiniz. Deneyimli bir ekip tarafından geliştirilmiş olan Avalon, size işinizi büyütmeniz ve verimliliğinizi artırmanız için gereken araçları sağlar." />
+            </Head>
+        <nav
+            className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+            <div className="px-3 py-3 lg:px-5 lg:pl-3">
+                <div className="flex items-center justify-between">
+                    <div className="flex items-center justify-start">
+                        <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar"
+                                onClick={sidebarHandle}
+                                aria-controls="logo-sidebar" type="button"
+                                className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                            <span className="sr-only">Open sidebar</span>
+                            <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path clip-rule="evenodd" fill-rule="evenodd"
+                                      d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+                            </svg>
+                        </button>
+                        <Link href="/" className="flex ml-2 md:mr-24">
+                            <img src="https://storage.googleapis.com/yunusemretopcu.appspot.com/logo.png" className="h-8 mr-3"
+                                 alt="avalon-logo" />
+                            <span
+                                className="self-center text-xl  font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Avalon Management Systems</span>
+                        </Link>
+                    </div>
+                    <div className="flex items-center">
+                        <div className="flex items-center ml-3">
+                            <div>
+                                <button type="button"
+                                        onClick={profileHandle}
+                                        className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                                        aria-expanded="false" data-dropdown-toggle="dropdown-user">
+                                    <span className="sr-only">Open user menu</span>
+                                    <img className="w-8 h-8 rounded-full"
+                                         src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                                         alt="user photo" />
+                                </button>
+                            </div>
+                            <div
+                                className={classNames("z-50 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600", {
+                                    "hidden": !profileToggle,
+                                    "fixed top-12 right-0": profileToggle
+                                })}
+                                id="dropdown-user">
+                                <div className="px-4 py-3" role="none">
+                                    <p className="text-sm text-gray-900 dark:text-white" role="none">
+                                        Neil Sims
+                                    </p>
+                                    <p className="text-sm font-medium text-gray-900 truncate dark:text-gray-300"
+                                       role="none">
+                                        neil.sims@flowbite.com
+                                    </p>
+                                </div>
+                                <ul className="py-1" role="none">
+                                    <li>
+                                        <a href="#"
+                                           className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                                           role="menuitem">Dashboard</a>
+                                    </li>
+                                    <li>
+                                        <a href="#"
+                                           className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                                           role="menuitem">Settings</a>
+                                    </li>
+                                    <li>
+                                        <a href="#"
+                                           className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                                           role="menuitem">Earnings</a>
+                                    </li>
+                                    <li>
+                                        <a href="#"
+                                           className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                                           role="menuitem">Sign out</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <aside id="logo-sidebar"
+               className= {classNames("fixed top-0 left-0 sm:-translate-x-0 z-40 w-64 h-screen pt-20 transition-transform  bg-white border-r border-gray-200 dark:bg-gray-800  dark:border-gray-700",
+                   {"-translate-x-full": sidebarToggle,
+                   })}
+               aria-label="Sidebar">
+            <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+                <ul className="space-y-2 font-medium">
+                    <li>
+                        <Link href="#"
+                              className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                            <svg aria-hidden="true"
+                                 className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                                 fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
+                                <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
+                            </svg>
+                            <span className="ml-3">Dashboard</span>
+                        </Link>
+                    </li>
+
+                </ul>
+            </div>
+        </aside>
+
+        <div className="p-4 sm:ml-64">
+            <div className="p-4 rounded-lg dark:border-gray-700 mt-14">
+                {children}
+            </div>
+        </div>
+        </>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import '@/styles/globals.css'
+import HomeLayout from '@/layouts/HomeLayout.js'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const getLayout = (Component) =>  {
+    if(Component.HomeLayout){
+      return HomeLayout;
+    }
+    return HomeLayout;
+  };
+  const Layout = getLayout(Component);
+return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+)
 }
