112f39081c610c833631e560114ac0235d1057f2
Yunus Emre Topçu <31772115+ynsmrtpc@users.noreply.github.com>
2023-07-04 16:11:03 +0300

    Update database.js

--- pages/api/database.js
+++ pages/api/database.js
@@ -1,6 +1,6 @@
 import {Sequelize} from 'sequelize';
 import pg from 'pg';
-
+// https://answers.netlify.com/t/cloud-function-cannot-connect-to-postgresql-with-sequelizejs-in-production-works-locally/50179
 export async function connectToDatabase() {
     const db = new Sequelize('postgres', 'postgres', process.env.DB_PASS, {
         host: process.env.DB_HOST,
