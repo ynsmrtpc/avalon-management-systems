b1e6f69ad4ca92af1b3456ee4c87d54fd9331295
ynsmrtpc <yunusemre_topcu@hotmail.com>
2023-07-13 19:39:13 +0300

    veritabanı adı güncellendi

--- pages/api/Models/SidebarModel.js
+++ pages/api/Models/SidebarModel.js
@@ -2,13 +2,13 @@ import { DataTypes } from "sequelize";
 import { connectToDatabase } from "@/pages/api/database";
 async function initialize() {
     const db = await connectToDatabase();
-    const SidebarMenu = db.define('admin_sidebar', {
+    const SidebarMenu = db.define('modules', {
         title: DataTypes.STRING,
         icon: DataTypes.STRING,
         link: DataTypes.STRING,
         queue: DataTypes.INTEGER,
     }, {
-        tableName: 'admin_sidebar',
+        tableName: 'modules',
         timestamps: false, // createdAt ve updatedAt sütunlarını devre dışı bırak
     });
     return {
