8f651042dcca0ebe8317cc999f9a5465d7a1c70e
ynsmrtpc <yunusemre_topcu@hotmail.com>
2023-07-17 01:54:26 +0300

    certification api allow cors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async rewrites() {
+    return [
+      {
+        source: '/api/certification*',
+        destination: 'https://api.yunusemretopcu.com/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
