07889275ff75fbb2c9b84972c3450b25bc3cb83e
ynsmrtpc <yunusemre_topcu@hotmail.com>
2023-08-07 01:33:14 +0300

    fixed bugs

--- utils/getUserData.js
+++ utils/getUserData.js
@@ -1,16 +1,18 @@
-    import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
-    export default async function getUserData(req) {
-        const cookies = req.headers.cookie;
-        const token = cookies?.split(';').find(cookie => cookie.trim().startsWith('login_token='))?.split('=')[1];
-        const secretKey = `${process.env.SECRET_KEY}`;
+export default async function getUserData(req) {
+  const cookies = req.headers.cookie;
+  const token = cookies
+    ? cookies
+        .split(";")
+        .find((cookie) => cookie.trim().startsWith("login_token="))
+        .split("=")[1]
+    : null;
+  const secretKey = `${process.env.SECRET_KEY}`;
 
-        try {
-            const decodedToken = jwt.verify(token, secretKey);
-            const { username } = decodedToken;
-            return username ;
-        } catch (error) {
-            return null;
-        }
-    };
-
+  try {
+    return jwt.verify(token, secretKey);
+  } catch (error) {
+    return null;
+  }
+}
