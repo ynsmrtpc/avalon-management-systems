1a186344a779ce4b7ef0c5d122af73b8df116a1b
Yunus Emre Topçu <31772115+ynsmrtpc@users.noreply.github.com>
2023-09-21 13:47:38 +0300

    Update Avatar.js

--- components/Avatar/Avatar.js
+++ components/Avatar/Avatar.js
@@ -1,9 +1,41 @@
-export default function Avatar({imageURL, altContent, size, rounded}) {
-    return (
-        <div className="avatar">
-            <div className={`w-${size} rounded-${rounded}`}>
-                <img src={imageURL} alt={altContent}/>
-            </div>
-        </div>
-    )
+export default function Avatar({ imageURL, altContent, size, rounded }) {
+
+    switch (size) {
+        case "lg":
+            return (
+                <div className="avatar">
+                    <div className={`w-32 rounded-${rounded}`}>
+                        <img src={imageURL} alt={altContent} />
+                    </div>
+                </div>
+            )
+            break;
+        case "md":
+            return (
+                <div className="avatar">
+                    <div className={`w-20 rounded-${rounded}`}>
+                        <img src={imageURL} alt={altContent} />
+                    </div>
+                </div>
+            )
+            break;
+        case "sm":
+            return (
+                <div className="avatar">
+                    <div className={`w-16 rounded-${rounded}`}>
+                        <img src={imageURL} alt={altContent} />
+                    </div>
+                </div>
+            )
+            break;
+        case "xs":
+            return (
+                <div className="avatar">
+                    <div className={`w-8 rounded-${rounded}`}>
+                        <img src={imageURL} alt={altContent} />
+                    </div>
+                </div>
+            )
+            break;
+    }
 }
